36de202683eb2dbadf77c00b7aa4f4bd293c0fa5
pmavr <p.mavrogiannis@outlook.com>
2022-05-21 17:27:22 +0300

    update camera tuner

--- camera_parameters/Camera.py
+++ camera_parameters/Camera.py
@@ -20,84 +20,61 @@ def edge_map_from_homography(homography, binary_court, edge_map_resolution):
 
 class Camera:
     def __init__(self, camera_params):
-        """
-        :param fl:
-        :param u:
-        :param v:
-        :param cc:
-        :param rod_rot:
-        """
-        u, v, fl = camera_params[0:3]
-        rod_rot = camera_params[3:6]
-        cc = camera_params[6:9]
+        self.image_center_x = camera_params[0]
+        self.image_center_y = camera_params[1]
+        self.focal_length = camera_params[2]
+        self.tilt_angle = camera_params[3]
+        self.pan_angle = camera_params[4]
+        self.roll_angle = camera_params[5]
+        self.camera_center_x = camera_params[6]
+        self.camera_center_y = camera_params[7]
+        self.camera_center_z = camera_params[8]
 
-        self.calibration_matrix_K = self._set_calibration(fl, u, v)
-        self.camera_center = self._set_camera_center(cc)
-        self.rotation = self._set_rotation(rod_rot)
-        self.projection_matrix_P = self._compute_matrix()
-
-    @staticmethod
-    def _set_calibration(fl, u, v):
-        return np.asarray([[fl, 0, u],
-                           [0, fl, v],
+    def calibration_matrix(self):
+        return np.array([[self.focal_length, 0, self.image_center_x],
+                           [0, self.focal_length, self.image_center_y],
                            [0, 0, 1]])
 
-    @staticmethod
-    def _set_camera_center(cc):
-        assert cc.shape[0] == 3
+    def camera_center(self):
         camera_center = np.zeros(3)
-        camera_center[0] = cc[0]
-        camera_center[1] = cc[1]
-        camera_center[2] = cc[2]
+        camera_center[0] = self.camera_center_x
+        camera_center[1] = self.camera_center_y
+        camera_center[2] = self.camera_center_z
         return camera_center
 
-    @staticmethod
-    def _set_rotation(rod_rot):
-        """
-        :param rod_rot: Rodrigues vector
-        :return:
-        """
-        assert rod_rot.shape[0] == 3
+    def rotation_matrix(self):
         rotation = np.zeros(3)
-        rotation[0] = rod_rot[0]
-        rotation[1] = rod_rot[1]
-        rotation[2] = rod_rot[2]
+        rotation[0] = self.tilt_angle
+        rotation[1] = self.pan_angle
+        rotation[2] = self.roll_angle
+        rotation, _ = cv2.Rodrigues(rotation)
         return rotation
 
-    def get_homography(self):
-        """
-        homography matrix from the projection matrix
-        :return:
-        """
-        h = self.projection_matrix_P[:, [0, 1, 3]]
+    def homography(self):
+        P = self.projection_matrix()
+        h = P[:, [0, 1, 3]]
         return h
 
-    def _compute_matrix(self):
-        P = np.zeros((3, 4))
-        for i in range(3):
-            P[i][i] = 1.0
-
-        for i in range(3):
-            P[i][3] = -self.camera_center[i]
-
-        r, _ = cv2.Rodrigues(self.rotation)
-        self.calibration_matrix_K = self.calibration_matrix_K
-        return self.calibration_matrix_K @ r @ P
+    def projection_matrix(self):
+        P = np.eye(3, 4)
+        P[:, 3] = -1*self.camera_center()
+        K = self.calibration_matrix()
+        R = self.rotation_matrix()
+        return K @ R @ P
 
     @staticmethod
-    def project_point_on_topview(point, h, f_w=1, f_h=1):
+    def project_point_on_topview(point, h, s_w=1, s_h=1):
         x, y = point
         w = 1.0
         p = np.zeros(3)
         p[0], p[1], p[2] = x, y, w
 
-        # flip template in y direction
-        m1 = np.asarray([[1, 0, 0],
-                         [0, -1, 68],
-                         [0, 0, 1]])
-        scale = np.asarray([[f_w, 0, 0],
-                            [0, f_h, 0],
-                            [0, 0, 1]])
+        m1 = np.array([[1, 0, 0],
+                       [0, -1, 68],
+                       [0, 0, 1]])
+        scale = np.array([[s_w, 0, 0],
+                          [0, s_h, 0],
+                          [0, 0, 1]])
         homography_matrix = h @ m1
         homography_matrix = homography_matrix @ scale
         inverted_homography_matrix = np.linalg.inv(homography_matrix)
--- camera_parameters/camera_tuning.py
+++ camera_parameters/camera_tuning.py
@@ -10,30 +10,30 @@ def normalize_in_range(value, min, max):
 
 def update_image(val):
 
-    focal_point = cv2.getTrackbarPos('focal_point', title_window)
-    rotation_0 = cv2.getTrackbarPos('rotation_0', title_window)
-    rotation_1 = cv2.getTrackbarPos('rotation_1', title_window)
-    rotation_2 = cv2.getTrackbarPos('rotation_2', title_window)
-    use_pan_angle = cv2.getTrackbarPos('use_pan_angle', title_window)
-    pan_angle = cv2.getTrackbarPos('pan_angle', title_window)
-    camera_loc_x = cv2.getTrackbarPos('camera_loc_x', title_window)
-    camera_loc_y = cv2.getTrackbarPos('camera_loc_y', title_window)
-    camera_loc_z = cv2.getTrackbarPos('camera_loc_z', title_window)
+    focal_point = cv2.getTrackbarPos('Focal length', title_window)
+    rotation_0 = cv2.getTrackbarPos('Tilt angle', title_window)
+    rotation_1 = cv2.getTrackbarPos('Pan angle', title_window)
+    rotation_2 = cv2.getTrackbarPos('Roll angle', title_window)
+    use_pan_angle = cv2.getTrackbarPos('use pan-roll movement', title_window)
+    pan_angle = cv2.getTrackbarPos('Pan-Roll angle', title_window)
+    camera_loc_x = cv2.getTrackbarPos('Camera loc x', title_window)
+    camera_loc_y = cv2.getTrackbarPos('Camera loc y', title_window)
+    camera_loc_z = cv2.getTrackbarPos('Camera loc z', title_window)
 
     focal_point = normalize_in_range(focal_point, 1463, 5696)
 
     rotation_0 = normalize_in_range(rotation_0, 1.60195, 1.83254)
     if use_pan_angle == 1:
-        angle_val = normalize_in_range(pan_angle, -0.55999, 0.55999)
+        angle_val = normalize_in_range(pan_angle, -0.60999, 0.60999)
         rotation_1 = angle_val
         rotation_2 = angle_val * (-1)
     else:
-        rotation_1 = normalize_in_range(rotation_1, -0.55999, 0.55999)
-        rotation_2 = normalize_in_range(rotation_2, -0.46343, 0.46286)
+        rotation_1 = normalize_in_range(rotation_1, -0.65999, 0.65999)
+        rotation_2 = normalize_in_range(rotation_2, -0.51343, 0.51286)
 
-    camera_loc_x = normalize_in_range(camera_loc_x, 45.05679, 60.84563)
+    camera_loc_x = normalize_in_range(camera_loc_x, 10., 90.)
     camera_loc_y = normalize_in_range(camera_loc_y, -66.07020, -16.74178)
-    camera_loc_z = normalize_in_range(camera_loc_z, 10.13871, 23.01126)
+    camera_loc_z = normalize_in_range(camera_loc_z, 0., 23.01126)
 
     camera_params = np.array([
         image_center_x,
@@ -47,10 +47,23 @@ def update_image(val):
         camera_loc_z
     ])
     camera = Camera(camera_params)
-    homography = camera.get_homography()
+    homography = camera.homography()
     edge_map = edge_map_from_homography(homography,
                                         binary_court,
                                         image_resolution)
+    text = f"focal length: {round(camera.focal_length, 3)} \n" \
+           f"cam_loc_X: {round(camera.camera_center_x, 3)} \n" \
+           f"cam_loc_Y: {round(camera.camera_center_y, 3)} \n" \
+           f"cam_loc_Z: {round(camera.camera_center_z, 3)} \n" \
+           f"tilt: {round(camera.tilt_angle * 180 / np.pi, 3)} \n" \
+           f"pan: {round(camera.pan_angle * 180 / np.pi, 3)} \n" \
+           f"roll: {round(camera.roll_angle * 180 / np.pi, 3)} \n"
+    y0, dy = 30, 20
+    for i, line in enumerate(text.split('\n')):
+        y = y0 + i * dy
+        cv2.putText(edge_map, line, (20, y),
+                    cv2.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255))
+    cv2.circle(edge_map, (int(camera.image_center_x), int(camera.image_center_y)), 2, (0, 255, 0), 3)
     cv2.imshow(title_window, edge_map)
 
 if __name__ == '__main__':
@@ -58,28 +71,28 @@ if __name__ == '__main__':
     image_center_x = image_resolution[0]/2
     image_center_y = image_resolution[1]/2
     focal_point = 0
-    rotation_0 = 0
-    rotation_1 = 50
-    rotation_2 = 50
+    tilt_angle = 0
     pan_angle = 50
+    roll_angle = 50
+    pan_roll_angle = 50
     camera_loc_x = 50
     camera_loc_y = 50
-    camera_loc_z = 0
+    camera_loc_z = 10
 
     title_window = 'Camera Parameters Tuning'
 
     binary_court = sio.loadmat('worldcup2014.mat')
 
     cv2.namedWindow(title_window)
-    cv2.createTrackbar('focal_point', title_window, focal_point, 100, update_image)
-    cv2.createTrackbar('rotation_0', title_window, rotation_0, 100, update_image)
-    cv2.createTrackbar('rotation_1', title_window, rotation_1, 100, update_image)
-    cv2.createTrackbar('rotation_2', title_window, rotation_2, 100, update_image)
-    cv2.createTrackbar('use_pan_angle', title_window, pan_angle, 1, update_image)
-    cv2.createTrackbar('pan_angle', title_window, pan_angle, 100, update_image)
-    cv2.createTrackbar('camera_loc_x', title_window, camera_loc_x, 100, update_image)
-    cv2.createTrackbar('camera_loc_y', title_window, camera_loc_y, 100, update_image)
-    cv2.createTrackbar('camera_loc_z', title_window, camera_loc_z, 100, update_image)
+    cv2.createTrackbar('Focal length', title_window, focal_point, 100, update_image)
+    cv2.createTrackbar('Tilt angle', title_window, tilt_angle, 100, update_image)
+    cv2.createTrackbar('Pan angle', title_window, pan_angle, 100, update_image)
+    cv2.createTrackbar('Roll angle', title_window, roll_angle, 100, update_image)
+    cv2.createTrackbar('use pan-roll movement', title_window, pan_roll_angle, 1, update_image)
+    cv2.createTrackbar('Pan-Roll angle', title_window, pan_roll_angle, 100, update_image)
+    cv2.createTrackbar('Camera loc x', title_window, camera_loc_x, 100, update_image)
+    cv2.createTrackbar('Camera loc y', title_window, camera_loc_y, 100, update_image)
+    cv2.createTrackbar('Camera loc z', title_window, camera_loc_z, 100, update_image)
     update_image(1)
 
     # Wait until user press some key
